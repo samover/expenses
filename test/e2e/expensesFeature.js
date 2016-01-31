7de671ee84c5fa8b0179d52ee24eb563d05cedfe
samover <samuel.overloop@gmail.com>
2016-01-31 20:37:01 +0100

    feature test for login/logout

--- test/e2e/expensesFeature.js
+++ test/e2e/expensesFeature.js
@@ -1,7 +1,21 @@
 describe('Expenses App', function() {
-  it('has a title', function() {
+  beforeEach(function() {
     browser.get('http://localhost:9292');
+  });
 
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Expenses App');
+  });
+
+  it('allows a user to log in', function() {
+    var loginBox = browser.findElement(by.id('loginBox'));
+    var logoutBox = browser.findElement(by.id('logoutBox'));
+
+    loginBox.sendKeys('Andrew');
+    loginBox.click();
+
+    expect(loginBox.isPresent()).toBe(false);
+    expect(logoutBox.isPresent()).toBe(true);
+    expect(logoutBox.getAttribute('value')).toEqual('Logout Andrew');
   });
 });
