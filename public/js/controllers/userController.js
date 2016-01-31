1955ba1bc05d073c7161257a299ec1f770dfc2af
samover <samuel.overloop@gmail.com>
2016-01-31 22:05:15 +0100

    add userController login and logout

--- public/js/controllers/userController.js
+++ public/js/controllers/userController.js
@@ -1,2 +1,16 @@
-app.controller('UserController', function() {
-});
+app.controller('UserController', ['Users', function(Users) {
+  var self = this;
+  Users.get(function(response) { self.users = response.users; });
+
+  self.loginUser = function(userName) {
+    self.currentUser = self.users.filter(function(user) {
+      return user.username === userName;
+    })[0];
+
+    return self.currentUser !== undefined;
+  }
+
+  self.logoutUser = function() {
+    self.currentUser = undefined;
+  }
+}]);
--- test/userController.spec.js
+++ test/userController.spec.js
@@ -5,5 +5,37 @@ describe('UserController', function() {
 
   beforeEach( inject( function( $controller ){
     ctrl = $controller( 'UserController' );
+
+    ctrl.users = [
+        { "id": 1, "username": "Terry", },
+        { "id": 2, "username": "Angel", },
+      ]
   }));
+
+  it('initializes with an empty user', function() {
+    expect(ctrl.currentUser).toBeUndefined();
+  });
+
+  describe('Logging in', function() {
+    it('returns true when logging in with existing user', function() {
+        expect(ctrl.loginUser('Terry')).toBe(true);
+    });
+
+    it('returns false when the user does not exist', function() {
+      expect(ctrl.loginUser('Ellen')).toBe(false);
+    });
+
+    it('currentUser is an object with the user data', function() {
+      ctrl.loginUser('Terry');
+      expect(ctrl.currentUser).toEqual(ctrl.users[0]);
+    });
+  });
+
+  describe('Logging out', function() {
+    it('destroys the current user', function() {
+      ctrl.loginUser('Terry');
+      ctrl.logoutUser('Terry');
+      expect(ctrl.currentUser).toBeUndefined();
+    });
+  });
 });
