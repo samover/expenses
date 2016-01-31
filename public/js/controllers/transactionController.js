1ea679fb99e73951fdffb8a17359bb23954be9cb
samover <samuel.overloop@gmail.com>
2016-01-31 17:27:45 +0100

    update to use expensesfactory

--- public/js/controllers/transactionController.js
+++ public/js/controllers/transactionController.js
@@ -1,47 +1,13 @@
-app.controller('TransactionController', [function() {
+app.controller('TransactionController', ['Expenses', function(Expenses) { 
   var self = this;
-  var transactions = {
-    "expenses": [
-    {
-      "id": 1,
-      "user_id": 1,
-      "amount": 14,
-      "note": "Ipsa eos qui id officia in.",
-      "category_id": 4,
-      "created_at": "2016-01-29T16:46:35.710Z",
-      "updated_at": "2016-01-29T16:46:35.710Z"
-    },
-    {
-      "id": 2,
-      "user_id": 1,
-      "amount": 15,
-      "note": "Quis eum voluptatum voluptate.",
-      "category_id": 3,
-      "created_at": "2016-01-29T16:46:35.718Z",
-      "updated_at": "2016-01-29T16:46:35.718Z"
-    },
-    {
-      "id": 3,
-      "user_id": 1,
-      "amount": 19,
-      "note": "Ducimus similique nostrum totam nihil aut.",
-      "category_id": 3,
-      "created_at": "2016-01-29T16:46:35.723Z",
-      "updated_at": "2016-01-29T16:46:35.723Z"
-    },
-    {
-      "id": 4,
-      "user_id": 2,
-      "amount": 19,
-      "note": "Arma virumque cano",
-      "category_id": 3,
-      "created_at": "2016-01-29T16:46:35.723Z",
-      "updated_at": "2016-01-29T16:46:35.723Z"
-    }
-    ]};
+  var transactions = Expenses.get(function(response) {
+    self.transactions = response.expenses;
+  });
 
   self.userExpenses = function() {
-    return transactions.expenses.filter(filterByUserId);
+    if (self.transactions !== undefined) {
+      return self.transactions.filter(filterByUserId);
+    }
   }
 
   function filterByUserId(obj) {
--- test/transactionController.spec.js
+++ test/transactionController.spec.js
@@ -1,11 +1,10 @@
 describe('TransactionController', function() {
   var ctrl, transactions;
 
-  beforeEach( module( 'Expenses' ));
+  beforeEach( module( 'ExpensesApp' ));
 
   beforeEach( inject( function( $controller ){
     ctrl = $controller( 'TransactionController' );
-
   }));
 
   it('Initialises with an empty transaction and daterange', function() {
@@ -16,39 +15,16 @@ describe('TransactionController', function() {
 
   describe('when user is logged in', function() {
     
-   var expenses = [
-      {
-        "id": 1,
-        "user_id": 1,
-        "amount": 14,
-        "note": "Ipsa eos qui id officia in.",
-        "category_id": 4,
-        "created_at": "2016-01-29T16:46:35.710Z",
-        "updated_at": "2016-01-29T16:46:35.710Z"
-      },
-      {
-        "id": 2,
-        "user_id": 1,
-        "amount": 15,
-        "note": "Quis eum voluptatum voluptate.",
-        "category_id": 3,
-        "created_at": "2016-01-29T16:46:35.718Z",
-        "updated_at": "2016-01-29T16:46:35.718Z"
-      },
-      {
-        "id": 3,
-        "user_id": 1,
-        "amount": 19,
-        "note": "Ducimus similique nostrum totam nihil aut.",
-        "category_id": 3,
-        "created_at": "2016-01-29T16:46:35.723Z",
-        "updated_at": "2016-01-29T16:46:35.723Z"
-      }
-    ];
+   var userExpenses = [ { "id": 1, "user_id": 1, }, { "id": 3, "user_id": 1, } ];
 
     it('shows the user\'s expenses', function() {
       ctrl.currentUser = 1;
-      expect(ctrl.userExpenses()).toEqual(expenses);
+      ctrl.transactions = [
+          { "id": 1, "user_id": 1, },
+          { "id": 2, "user_id": 2, },
+          { "id": 3, "user_id": 1, }
+      ];
+      expect(ctrl.userExpenses()).toEqual(userExpenses);
     });
   });
 });
