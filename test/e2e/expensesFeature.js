dfb4170beb30010cb902edb437d3216addafbf6e
samover <samuel.overloop@gmail.com>
2016-02-01 11:40:46 +0100

    upon login page should show user expenses

--- test/e2e/expensesFeature.js
+++ test/e2e/expensesFeature.js
@@ -1,4 +1,6 @@
 describe('Expenses App', function() {
+  var authenticate;
+
   beforeEach(function() {
     browser.get('http://localhost:9292');
   });
@@ -17,11 +19,25 @@ describe('Expenses App', function() {
     username.sendKeys('Miles');
     login.click();
 
-    browser.driver.sleep(2000);
-
     expect(loginBox.isDisplayed()).toBeFalsy();
 
     var logoutText = browser.findElement(by.linkText('Logout Miles'));
     expect(logoutText.isDisplayed()).toBeTruthy();
   });
+
+  it('shows a user\'s expenses upon login', function() {
+    authenticate('Miles');
+
+    var logoutText = browser.findElement(by.linkText('Logout Miles'));
+    expect(logoutText.isDisplayed()).toBeTruthy();
+    var expenses = element.all(by.repeater("expense in transCtrl.userExpenses()"));
+    expect(expenses.length).toBeGreaterThan(0);
+  });
+  
+  authenticate = function(user) {
+    var username = browser.findElement(by.model('userName'));
+    var login = browser.findElement(by.buttonText('Login'));
+    username.sendKeys(user);
+    login.click();
+  }
 });
