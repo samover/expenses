984982973078b949b998a697886c7f6c7aee7349
samover <samuel.overloop@gmail.com>
2016-02-01 11:01:08 +0100

    extract all html into partials

--- public/js/expensesDirectives.js
+++ public/js/expensesDirectives.js
@@ -4,3 +4,24 @@ app.directive('expensesHeader', function() {
     templateUrl: 'partials/expenses-header.html'
   }
 });
+
+app.directive('expensesAside', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/expenses-aside.html'
+  }
+});
+
+app.directive('expensesArticle', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/expenses-article.html'
+  }
+});
+
+app.directive('expensesFooter', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/expenses-footer.html'
+  }
+});
