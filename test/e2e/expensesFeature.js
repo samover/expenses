b4702ac4fb47dacc54e1650429882468de7328a4
samover <samuel.overloop@gmail.com>
2016-02-01 11:24:29 +0100

    pass first feature test

--- test/e2e/expensesFeature.js
+++ test/e2e/expensesFeature.js
@@ -9,14 +9,19 @@ describe('Expenses App', function() {
 
   it('allows a user to log in', function() {
     var loginBox = browser.findElement(by.id('loginBox'));
-    var logoutText = browser.findElement(by.linkText('Logout Andrew'));
+    var username = browser.findElement(by.model('userName'));
+    var login = browser.findElement(by.buttonText('Login'));
 
     expect(loginBox.isDisplayed()).toBeTruthy();
 
-    loginBox.sendKeys('Andrew');
-    loginBox.click();
+    username.sendKeys('Miles');
+    login.click();
+
+    browser.driver.sleep(2000);
 
     expect(loginBox.isDisplayed()).toBeFalsy();
+
+    var logoutText = browser.findElement(by.linkText('Logout Miles'));
     expect(logoutText.isDisplayed()).toBeTruthy();
   });
 });
